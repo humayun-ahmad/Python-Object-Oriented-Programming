1ca69c5ba0fba448a910fdb869dc251deed2651a
humayun-ahmad <humayun.ahmad.rajib@gmail.com>
2024-12-04 22:14:24 +0600

    Update(main.py): main file has been updated

--- Practice/main.py
+++ Practice/main.py
@@ -2,6 +2,9 @@ from car import Car
 from student import Student
 from animal import Dog, Cat, Mouse, Animal
 from multiple_inheritance import Animal, Prey, Predator, Rabbit, Hawk, Fish
+from composition import Engine, Wheel, Car
+
+
 
 # car1 = Car("Mustang", 2024, "red", False)
 # car2 = Car("Toyota", 2024, "red", False)
@@ -42,5 +45,12 @@ from multiple_inheritance import Animal, Prey, Predator, Rabbit, Hawk, Fish
 # hawk.hunt()
 # fish.hunt()
 # fish.flee()
+
+
+
+# car1 = Car(make="Ford", model="Mustang", horse_power=500, wheel_size=18)
+# car2 = Car(make="Chevrolet", model="Corvette", horse_power=800, wheel_size=19)
+
+# print(car1.display_car())
 
 
